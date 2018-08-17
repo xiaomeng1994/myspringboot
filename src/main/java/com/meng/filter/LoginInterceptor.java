a7140c489312399476648cd9f79546a75c0caf96
mengzl <mzl123456>
2018-08-17 14:34:21 +0800

    添加拦截器

---
+++ src/main/java/com/meng/filter/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.meng.filter;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String token = httpServletRequest.getHeader("token");
+        if(StringUtils.isEmpty(token)){
+            return false;
+        }
+        httpServletRequest.setAttribute("token",token);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
+
+}
---
+++ src/main/java/com/meng/filter/WebMvcConfigure.java
@@ -0,0 +1,20 @@
+package com.meng.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigure extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 多个拦截器组成一个拦截器链
+        // addPathPatterns 用于添加拦截规则
+        // excludePathPatterns 用户排除拦截
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/api/**");
+        //registry.addInterceptor(new MyInterceptor2()).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+
+}
