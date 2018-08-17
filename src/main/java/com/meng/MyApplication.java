bab6eec054a40b13e73ae641aec750128afe565b
mengzl <mzl123456>
2018-08-17 12:17:40 +0800

    添加swagger

--- src/main/java/com/meng/MyApplication.java
+++ src/main/java/com/meng/MyApplication.java
@@ -1,25 +1,21 @@
 package com.meng;
 
-import com.meng.filepath.service.FilePathService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableScheduling//启动定时任务
+@EnableSwagger2             //启动swagger注解
 //@ComponentScan(basePackages = "com.meng")
 //@EnableAutoConfiguration
 public class MyApplication {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(MyApplication.class, args);
-        FilePathService filePathService = (FilePathService) context.getBean("filePathService");
-        //String path = "C:\\Users\\1\\Desktop\\数据3";
-        //String path = "C:\\Users\\1\\Desktop\\数据2\\城乡建设环境与资源保护工委\\市国土规划委\\控制性详细规划备案\\2016年度\\1.华南理工大学五山校区（天河区AT0501、AT0504、AT0505规划管理单元）";
-        String path = "C:\\Users\\1\\Desktop\\最终数据";
-        //filePathService.clearFileContentByPath(path);
-        filePathService.insertFilePathBydir(path);
-        //filePathService.insertFilePathBydir("C:\\Users\\1\\Desktop\\ztmlsjzl\\lyhylj\\lyylzdstgcjzqk");
-        SpringApplication.exit(context);
+
     }
 
 }
--- src/main/java/com/meng/user/controller/UserController.java
+++ src/main/java/com/meng/user/controller/UserController.java
@@ -4,41 +4,39 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.meng.user.service.impl.UserServiceImpl;
 import com.meng.user.vo.User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-@Controller
+@Api(value = "/api/user",description = "用户管理接口",tags = "UserController")
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
     @Resource(name = "userService")
     private UserServiceImpl userService;
 
-    @RequestMapping(value = "getUserById",params = {"userId"})
-    @ResponseBody
-    public User getUserById(String userId){
-        System.out.println(111);
+    @ApiOperation(value = "查询用户",notes = "根据用户ID获取用户")
+    @ApiParam(required = true,name = "userId",value = "用户id")
+    @RequestMapping(method = RequestMethod.GET,value = "getUserById/{userId}")
+    public User getUserById(@PathVariable String userId){
         return userService.getUserById(userId);
     }
 
-    @RequestMapping(value = "getUserList")
-    @ResponseBody
-    public PageInfo<User>  getUserList(Model model,
-             @RequestParam(name = "currentPage",value = "1") int currentPage,
-             @RequestParam(name = "pageSize",value = "10") int pageSize){
+    @ApiOperation(value = "获取用户列表信息")
+    @RequestMapping(method = RequestMethod.POST,value = "getUserList")
+    public PageInfo<User>  getUserList(
+             @RequestParam(name = "currentPage",defaultValue = "1") int currentPage,
+             @RequestParam(name = "pageSize",defaultValue = "10") int pageSize){
         PageHelper.startPage(currentPage,pageSize);
         List<User> userList = userService.getUserList();
         return new PageInfo<>(userList);
     }
 
-    @RequestMapping("/")
-    public String index(){
-        return "/UserList";
-    }
+
 
 }
